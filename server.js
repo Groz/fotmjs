4fd2e51c0a050dc59d8d08181867761bef03804f
Tagir Magomedov <tmagomedov@twitter.com>
2015-01-05 18:13:31 -0800

    Add logging

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
+  console.log('Connection initiated...');
+
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("Hello, Google Compute world!");
+
+  console.log('Request served.');
 }).listen(8080, '0.0.0.0');
