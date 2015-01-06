d5fdba6250cc4f632de0adaf43872835e9c1c490
Tagir Magomedov <tmagomedov@twitter.com>
2015-01-05 18:10:03 -0800

    Listen to outside connections

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("Hello, Google Compute world!");
-}).listen(8080);
+}).listen(8080, '0.0.0.0');
