b89540b290ca53453a4c8efe1357618479f2b70b
Tagir Magomedov <tmagomedov@twitter.com>
2015-01-22 14:26:24 -0800

    Minor refactoring. Replace underscore with lodash

--- server.js
+++ server.js
@@ -1,11 +1,64 @@
 var http = require('http');
+var app = http.createServer(handler);
+var io = require('socket.io')(app);
 var fs = require('fs');
 
-http.createServer(function (req, res) {
+app.listen(8080, '0.0.0.0');
+
+var indexPage;
+
+function handler(req, res) {
   console.log('Connection initiated...');
 
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end("Hello, Google Compute world!");
+  function renderIndex() {
+    res.writeHead(200);
+    res.end(indexPage);
+  }
+
+  function loadIndex(callback) {
+    fs.readFile(__dirname + '/index.html', function (err, data) {
+      if (err) {
+        indexPage = null;
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+
+      indexPage = data;
+      return callback();
+    });
+  }
+
+  if (!indexPage) {
+    console.log('Loading index page...');
+    loadIndex(renderIndex);
+  }
+  else {
+    console.log('Rendering index page...');
+    renderIndex();
+  }
 
   console.log('Request served.');
-}).listen(8080, '0.0.0.0');
+};
+
+var nUsers = 0;
+
+io.on('connection', function (socket) { // called for each user connection
+  console.log("SOCKET.IO: User connection initiated.")
+
+  function notifyUsers() {
+    io.sockets.emit('userCount', { total: nUsers });
+  }
+
+  socket.on('user connected', function (data) {
+    ++nUsers;
+    console.log("User connected. Total:", nUsers);
+    notifyUsers();
+  });
+
+  socket.on('disconnect', function () {
+    --nUsers;
+    console.log("User disconnected. Total:", nUsers);
+    notifyUsers();
+  });
+
+});
